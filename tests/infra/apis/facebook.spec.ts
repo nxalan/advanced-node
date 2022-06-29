c10e892b24ea23f339b011030aed800113698068
Alan Nicolas de Oliveira <aoliveira.external@axa-assistance.us>
2022-06-29 20:13:44 -0300

    improve code quality

--- tests/infra/apis/facebook.spec.ts
+++ tests/infra/apis/facebook.spec.ts
@@ -15,7 +15,10 @@ describe('FacebookApi', () => {
   })
 
   beforeEach(() => {
-    httpClient.get.mockResolvedValueOnce({ access_token: 'any_app_token' })
+    httpClient.get
+      .mockResolvedValueOnce({ access_token: 'any_app_token' })
+      .mockResolvedValueOnce({ data: { user_id: 'any_user_id' } })
+      .mockResolvedValueOnce({ id: 'any_fb_id', name: 'any_fb_name', email: 'any_fb_email' })
     sut = new FacebookApi(httpClient, clientId, clientSecret)
   })
 
@@ -39,6 +42,26 @@ describe('FacebookApi', () => {
         access_token: 'any_app_token',
         input_token: 'any_client_token'
       }
+    })
+  })
+
+  it('should get user info', async () => {
+    await sut.loadUser({ token: 'any_client_token' })
+    expect(httpClient.get).toHaveBeenCalledWith({
+      url: 'https://graph.facebook.com/any_user_id',
+      params: {
+        fields: 'id,name,email',
+        access_token: 'any_client_token'
+      }
+    })
+  })
+
+  it('Should return facebook user', async () => {
+    const fbUser = await sut.loadUser({ token: 'any_client_token' })
+    expect(fbUser).toEqual({
+      facebookId: 'any_fb_id',
+      name: 'any_fb_name',
+      email: 'any_fb_email'
     })
   })
 })
