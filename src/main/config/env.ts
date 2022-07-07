8d5bc33093ce419d432b05474ef1978bee099454
Alan Nicolas de Oliveira <aoliveira.external@axa-assistance.us>
2022-07-06 21:33:22 -0300

    add Facebook Login factories

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -3,5 +3,6 @@ export const env = {
     clientId: process.env.FB_CLIENT_ID ?? '560181972319994',
     clientSecret: process.env.FB_CLIENT_SECRET ?? 'b03d0d125ffa3ed22e08a450e0147bc0'
   },
-  port: process.env.PORT ?? 8080
+  port: process.env.PORT ?? 8080,
+  jwtSecret: process.env.JWT_SECRET ?? '2j3j2382jd'
 }
