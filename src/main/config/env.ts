e57da448c5182673fd19eba8b1e56cb2a95de385
Alan Nicolas de Oliveira <aoliveira.external@axa-assistance.us>
2022-07-06 21:10:15 -0300

    configure express server

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -2,5 +2,6 @@ export const env = {
   facebookApi: {
     clientId: process.env.FB_CLIENT_ID ?? '560181972319994',
     clientSecret: process.env.FB_CLIENT_SECRET ?? 'b03d0d125ffa3ed22e08a450e0147bc0'
-  }
+  },
+  port: process.env.PORT ?? 8080
 }
